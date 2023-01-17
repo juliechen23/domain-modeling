da46c9a185bd55f56967708131af7ab66413ceb4
juliechen23 <julie.chen@kellogg.northwestern.edu>
2023-01-17 22:17:45 +0000

    created crm

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,50 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
--- CREATE TABLES
+-- CREATE CONTACTS TABLE
+DROP TABLE IF EXISTS contacts;
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone TEXT,
+  title TEXT,
+  company_id TEXT
+);
+
+-- CREATE ACTIVITIES TABLE
+DROP TABLE IF EXISTS activities;
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  contact_id TEXT,
+  type TEXT,
+  date TEXT,
+  time TEXT,
+  notes TEXT,
+  salesperson_id TEXT
+);
+
+-- CREATE COMPANIES TABLE
+DROP TABLE IF EXISTS companies;
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_name TEXT
+);
+
+-- CREATE SALESPEOPLE TABLE
+DROP TABLE IF EXISTS salespeople;
+CREATE TABLE salespeople (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+-- CREATE INDUSTRIES TABLE
+DROP TABLE IF EXISTS industries;
+CREATE TABLE industries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  industry TEXT,
+  company_id TEXT
+);
--- school/school.sql
+++ school/school.sql
@@ -13,7 +13,49 @@ CREATE TABLE students (
   email TEXT,
   phone_number TEXT
 );
+-- Insert student data
+INSERT INTO students ("first_name", "last_name", "email", "phone_number") VALUES ("Jane", "Doe", "jane@example.com","555-1212"); 
+INSERT INTO students ("first_name", "last_name", "email", "phone_number") VALUES ("Jenny", "Smith", "jenny@gmail.com","867-5309"); 
+INSERT INTO students ("first_name", "last_name", "email", "phone_number") VALUES ("John", "Johnson", "john@acme.com","456-7890"); 
 
 -- Create the rest of the tables
+-- Teacher table
+CREATE TABLE teachers (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT,
+last_name TEXT,
+bio TEXT);
+-- Insert teacher data
+INSERT INTO teachers ("first_name", "last_name","bio") VALUES ("Ben", "Block", "A seasoned pro instructor.");
+INSERT INTO teachers ("first_name", "last_name","bio") VALUES ("Brian", "Eng", "Loves tacos.");
 
+-- Course table
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+-- Insert course data
+INSERT INTO courses ("name", "description") VALUES ("Intro to Software Development","This course is focused on software development...");
+INSERT INTO courses ("name", "description") VALUES ("Taco-Making 101","In this course, you'll learn how to build a proper taco...");
 
+-- Section table
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id TEXT,
+  teacher_id TEXT
+);
+-- Insert section data
+INSERT INTO sections ("time","course_id","teacher_id") VALUES ("Tuesday 8:30-11:30","1","2");
+INSERT INTO sections ("time","course_id","teacher_id") VALUES ("Wednesday 6-9pm","1","1");
+INSERT INTO sections ("time","course_id","teacher_id") VALUES ("Wednesday 6-9pm","2","2");
+INSERT INTO sections ("time","course_id","teacher_id") VALUES ("Thursday 6-9pm","2","1");
+
+-- Enrollment table
+CREATE TABLE enrollments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  section_id TEXT,
+  student_id TEXT,
+  status TEXT
+);
